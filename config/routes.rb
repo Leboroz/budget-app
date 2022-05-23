0b3f7b61d37643506f12e76dafa1cbabbac78d5c
Leonardo Albornoz <albornozleo95@gmail.com>
2022-05-23 15:16:38 -0400

    Create devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "users#index"
+   root "groups#index"
+   resources :groups, only: %i[index]
 end
