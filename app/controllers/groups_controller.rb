ec355d6111e6894e9440a955868224303f1df475
Leonardo Albornoz <albornozleo95@gmail.com>
2022-05-24 11:39:34 -0400

    Add home page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,24 @@
 class GroupsController < ApplicationController
   def index
+    @groups = Group.all
+  end
+
+  def create
+    new_group = Group.new(groups_params)
+    new_group.author = current_user
+    respond_to do |format|
+      format.html do
+        if new_group.save
+          flash[:success] = 'Your category has been created'
+          redirect_back fallback_location: '/'
+        end
+      end
+    end
+  end
+
+  private
+
+  def groups_params
+    params.require(:category).permit(:name, :icon)
   end
 end
