155b8ebb58e5269e4bee188e60b97ad84af5c5b6
Leonardo Albornoz <albornozleo95@gmail.com>
2022-05-25 16:32:44 -0400

    Add categories to form

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,16 @@
 module GroupsHelper
+
+  def icons
+    {
+      'Accounts' => 'bank2',
+      'Crypto' => 'coin',
+      'Savings' => 'safe-fill',
+      'shipping' => 'truck',
+      'Fun' => 'balloon-fill',
+      'Media' => 'boombox-fill',
+      'Packages' => 'box2-fill',
+      'Work' => 'building',
+      'Shopping' => 'cart-fill'
+    }
+  end
 end
